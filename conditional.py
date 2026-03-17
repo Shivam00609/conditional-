1133ab10fa2994b5a2469ae6162299424e272f42
Shivam00609 <shivammaurya6025@gmail.com>
2026-03-17 14:43:52 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age: "))
+print("Your age is: ", age)
+if age < 13:
+    print("you are a child")
+elif age < 20:
+    print("you are Teenager")
+elif age < 60 :
+    print("you are a responsible Adult")
+else:
+    print("you are a Senior citizen")
